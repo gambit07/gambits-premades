7a213c73bbbe7eab85e1336f233cadde9fa8f977
Gambit <kbelite8@gmail.com>
2024-01-29 14:21:27 -0500

    0.0.19

--- .github/workflows/get-version.js
+++ .github/workflows/get-version.js
@@ -1,14 +1,2 @@
 let fs = require('fs')
-
-// Read the current version from module.json
-const moduleData = JSON.parse(fs.readFileSync('./module.json', 'utf8'));
-const currentVersion = moduleData.version;
-
-// Increment the version by 0.1
-const newVersion = (parseFloat(currentVersion) + 0.1).toFixed(2);
-
-// Update the version in module.json
-moduleData.version = newVersion;
-fs.writeFileSync('./module.json', JSON.stringify(moduleData, null, 2));
-
 console.log(JSON.parse(fs.readFileSync('./module.json', 'utf8')).version)
