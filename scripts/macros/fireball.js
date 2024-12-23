371a19662b571e2bf4cf8a69c43ab0be53b10d51
bakanabaka <3474820+aljames-arctic@users.noreply.github.com>
2024-12-23 17:29:56 -0600

    Update fireball.js
    Animate with or without targets, and right before damage is assigned

--- scripts/macros/fireball.js
+++ scripts/macros/fireball.js
@@ -1,15 +1,25 @@
-/* DIME entry only -- not integrated into game.gps */
+// Animate at the same time damage is applied
+async function preDamageApplication() {
+    if (workflow.fireballanimated) return;
+    await renderExplosion();
+}
 
-async function postDamageRoll() {
+// Animate even if there are no targets
+async function preActiveEffects() {
+    if (workflow.fireballanimated) return;
+    await renderExplosion();
+}
+
+async function renderExplosion() {
     let { animEnabled, damageColor } = game.gps.getCprConfig({itemUuid: macroItem.uuid});
-    if(!animEnabled) return; 
+    if (!animEnabled) return; 
 
     const damageType = workflow.damageDetail[0]?.type ?? "fire";
     damageColor = damageColor ?? "orange";
     if (["radiant", "lightning", "cold"].includes(damageType)) damageColor = "blue";
     else if (["necrotic", "poison", "acid"].includes(damageType)) damageColor = "green";
     else if (["thunder", "force", "psychic"].includes(damageType)) damageColor = "purple";
-    else if (["slashing", "bludgeoning", "piercing"].includes(damageType)) damageColor = "red";
+    else if (["slashing", "bludgeoning", "piercing"].includes(damageType)) damageColor = "purple";
 
     const templateData = fromUuidSync(workflow.templateUuid);
     let cast = game.gps.animation.fireball.cast(token, templateData, {color: damageColor});
@@ -17,10 +27,11 @@ async function postDamageRoll() {
 
     await cast.play();
     await explosion.play();
+    workflow.fireballanimated = true;
 }
 
 try {
-    let states = {postDamageRoll};
+    let states = {preActiveEffects, preDamageApplication};
     if (typeof workflow != "undefined") await states[workflow.macroPass]();
     else await states[args[0]]();
 } catch(e) { console.error(e); }
